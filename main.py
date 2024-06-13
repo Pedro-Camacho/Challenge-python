e91ba16f30c942a6a2844107688e105e71805254
Pedro Camacho <pedroalmeidacamacho@gmail.com>
2024-06-13 13:32:12 -0400

    projeto bem montado até agora esperando atualizações :)

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import random
-jogador = ['Jogador do Momento', 100]
-
 pilotos = ['Nick Cassidy', 'Pascal Wehrlein', 
            'Mitch Evans', 'Oliver Rowland', 
            'Jake Dennis', 'Jean-Éric Vergne', 
@@ -11,66 +9,80 @@ pilotos = ['Nick Cassidy', 'Pascal Wehrlein',
            'Robin Frijns', 'Nico Muller',
            'Dan ticktum', 'Sérgio Sette Câmara',
            'Jeahan Daruvala', 'Edoardo Mortara',
-           'Nyck De Vries', ' Joel, Eriksson',
+           'Nyck De Vries', ' Joel Eriksson',
            'Lucas di Grassi', 'Kelvin Van Der Linde',
            'Jordan King', 'Paul Aron' ]
 
 
-def abrir_pacote():
-    piloto_sorteado_1 = random.choice(pilotos)
-    raridada_piloto_1 = random.randrange(0,3)
-    
-    piloto_sorteado_2 = random.choice(pilotos)
-    raridada_piloto_2 = random.randrange(0,3)
-    
-    piloto_sorteado_3 = random.choice(pilotos)    
-    raridada_piloto_3 = random.randrange(0,3)
-    
-    jogador.append(piloto_sorteado_1)
-    jogador.append(piloto_sorteado_2)
-    jogador.append(piloto_sorteado_3)
-    
-    print('Parabens você ganhou:\n'
-          f'{piloto_sorteado_1} raridade: {raridada_piloto_1}\n'
-          f'{piloto_sorteado_2} raridade: {raridada_piloto_2}\n'
-          f'{piloto_sorteado_3} raridade: {raridada_piloto_3}\n')
+def abrir_pacote(jogador):
+    if jogador[1] >= 20:
+        piloto_sorteado_1 = random.choice(pilotos)
+        raridada_piloto_1 = random.randrange(1,4)
+        
+        piloto_sorteado_2 = random.choice(pilotos)
+        raridada_piloto_2 = random.randrange(1,4)
+        
+        piloto_sorteado_3 = random.choice(pilotos)    
+        raridada_piloto_3 = random.randrange(1,4)
+        
+        jogador.append(piloto_sorteado_1)
+        jogador.append(piloto_sorteado_2)
+        jogador.append(piloto_sorteado_3)
+        
+        print('Parabens você ganhou:\n'
+            f'{piloto_sorteado_1} raridade: {raridada_piloto_1}\n'
+            f'{piloto_sorteado_2} raridade: {raridada_piloto_2}\n'
+            f'{piloto_sorteado_3} raridade: {raridada_piloto_3}\n')
+        
+        jogador[1]-=20
+        print(f'{jogador[1]}')
+    else:
+        print(f'\nSaldo insuficiente {jogador[1]}\n')
     
 def vender_item(item_vender):
-    jogador[item_vender].pop
-    print(jogador)
+    while not item_vender in jogador:
+        item_vender=input("Digite um item real:")
+    jogador.remove(item_vender)
+    jogador[1]+=10
     
 def mostrar_colecao():
+    print(f'Esta é sua coleção {jogador[0]}')
     for i in range(len(jogador)):
         print(f'{jogador[i]}\n')
     
+def adicionar_fundos(valor):
+    jogador[1]+=valor
+    print(f"Valor adicionado a carteira ({valor})")
     
-    
-print("Bem vindo ao E-kids aqui você poderá abrir pacotes para sua coleção vendelos e trocar eles")
+def num_verificado(msg):
+    num = input(msg)
+    while not  num.isnumeric():
+        num=input(msg)
+    num= int(num)
+    return num
+
+print("Bem vindo ao E-kids aqui você poderá abrir pacotes para sua coleção vendelos e trocar eles\n")
+jogador=[]
+jogador.append(input("Digite seu Nome:\n"))
+jogador.append(num_verificado('Digite o valor do saldo:\n'))
 
 while True:
-    opcao=input("Escolha qual opração deseja: \n1-Abrir pacote.\n2-Vender item.\n3-\n4-Mostrar Coleção\n 5- Adicionar Moedas")
-    while not opcao.isnumeric():
-        opcao=input("Escolha qual opração deseja: \n1-Abrir pacote.\n2-Vender item.\n3-Mostrar Coleção\n 4- Adicionar Moedas")
-    opcao=int(opcao)
+    opcao=num_verificado("Escolha qual opração deseja: \n1-Abrir pacote.\n2-Vender item.\n3-Mostrar Coleção\n4-Adicionar Moedas")
 
     if opcao == 0:
         break
-    elif opcao == 1:
-        abrir_pacote()
-        if jogador[2]>20:
-            abrir_pacote()
-            jogador[2]-20
-        else:
-            print("Saldo insuficiente")
+    elif opcao == 1:                    
+        abrir_pacote(jogador)
     elif opcao == 2:
-        item_vender=input("O nome do item que você deseja vender")
+        for i in range(len(jogador)):
+            print(f'{jogador[i]}, ')
+        item_vender=input("O nome do item que você deseja vender: ")
         vender_item(item_vender)
     elif opcao == 3:
         mostrar_colecao()
         print("oi")
     elif opcao == 4:
-        #adicionar_fundos()
-        print("oi")
+        valor=num_verificado("Digite quanto de saldo deseja adicionar:")
+        adicionar_fundos(valor)       
     else:
-        
         print("Digite uma opcao valida!")
